325623717889d98ddfec92509b15f558c1167a85
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-12 16:22:37 -0600

    Update Sheuly.java
    ast

--- Sapna/src/Chitra/Sheuly.java
+++ Sapna/src/Chitra/Sheuly.java
@@ -4,6 +4,9 @@ public class Sheuly {
 
 	public static void main(String[] args) {
 		System.out.println("This is my trying for git ");
+		System.out.println("This is my trying for git- i am from remot ripo ");
+		System.out.println("This is my trying for git - i am from remote ripo");
+		
 
 	}
 
